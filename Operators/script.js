00b894ef8e85f35a38e4ec36f0f054c8d1e9c32a
pokaljainam24 <pokaljainam@gmail.com>
2024-06-10 12:06:26 +0530

    done

---
+++ Operators/script.js
@@ -0,0 +1,52 @@
+// let x = 5;
+// console.log(x != 6) // true
+// console.log(x != 5) // false
+// let y = 4;
+// console.log(x !== y) // true
+
+
+// console.log(1 !== 1);
+// // Expected output: false
+
+// console.log('hello' !== 'hello');
+// // Expected output: false
+
+// console.log('1' !== 1);
+// // Expected output: true
+
+// console.log(0 !== false);
+// // Expected output: true
+
+
+// x !== y;
+
+// !(x === y); // x is not define
+
+//-----------------------operator(!==)
+
+// const object1 = {
+//     key: "value",
+//   };
+  
+//   const object2 = {
+//     key: "value",
+//   };
+  
+//   console.log(object1 !== object2); // true
+//   console.log(object1 !== object1); // false
+
+
+//-----------------------operator(===)
+
+// const object1 = {
+//     key: "value",
+//   };
+  
+//   const object2 = {
+//     key: "value",
+//   };
+  
+//   console.log(object1 === object2); // false
+//   console.log(object1 === object1); // true
+  
+  
