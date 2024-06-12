c9653f986c992f482c99b59c6b78fdb70718efb4
pokaljainam24 <pokaljainam@gmail.com>
2024-06-12 10:01:54 +0530

    done

--- Operators/script.js
+++ Operators/script.js
@@ -1,3 +1,67 @@
+// Types of JavaScript Operators:
+
+// 1.Arithmetic Operators
+// 2.Assignment Operators
+// 3.Comparison Operators
+// 4.String Operators
+// 5.Logical Operators
+// 6.Bitwise Operators
+// 7.Ternary Operators
+// 8.Type Operators
+
+// 1.Arithmetic Operators
+
+        // +  --> Addition
+        // -  --> Subtraction
+        // *  --> Multiplication
+        // ** --> Exponentiation (ES2016)
+        // /  --> Division
+        // %  --> Modulus (Division Remainder)
+        // ++ --> Increment
+        // -- --> Decrement
+
+// 2.Assignment Operators
+
+        // =	--> x = y	x = y
+        // +=	--> x += y	x = x + y
+        // -=	--> x -= y	x = x - y
+        // *=	--> x *= y	x = x * y
+        // /=	--> x /= y	x = x / y
+        // %=	--> x %= y	x = x % y
+        // **=	--> x **= y	x = x ** y
+
+// 3.Comparison Operators
+
+        // ==	--> equal to
+        // ===	--> equal value and equal type
+        // !=	--> not equal
+        // !==	--> not equal value or not equal type
+        // >	--> greater than
+        // <	--> less than
+        // >=	--> greater than or equal to
+        // <=	--> less than or equal to
+        // ?	--> ternary operator
+
+// 4.Logical Operators
+
+        // &&	--> logical and
+        // ||	--> logical or
+        // !	--> logical not
+
+// 5.Bitwise Operators
+                                                                                
+        // &	--> AND	                    5 & 1	    0101 & 0001	    0001	 1
+        // |	--> OR	                    5 | 1	    0101 | 0001	    0101	 5
+        // ~	--> NOT	                    ~ 5	        ~0101	        1010	 10
+        // ^	--> XOR	                    5 ^ 1	    0101 ^ 0001	    0100	 4
+        // <<	--> left shift	            5 << 1	    0101 << 1	    1010	 10
+        // >>	--> right shift	            5 >> 1	    0101 >> 1	    0010	 2
+        // >>>	--> unsigned right shift	5 >>> 1	    0101 >>> 1	    0010	 2
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+
 // let x = 5;
 // console.log(x != 6) // true
 // console.log(x != 5) // false
