dffef85263fa994fea8a17ff389acf0a65ab3a2d
pokaljainam24 <pokaljainam@gmail.com>
2024-06-14 10:20:45 +0530

    done

---
+++ projecvt-1/script.js
@@ -0,0 +1,42 @@
+// Que-1 Javascript program to swape 2 variables.
+// Que-2 Javascript program find to square.
+// Que-3 Find the therd angle of a right triangle if to others angle are given.
+
+
+// Que-1 Javascript program to swape 2 variables.
+
+let a = 5;
+let b = 10;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`a: ${a}, b: ${b}`); // a: 10, b: 5
+
+let j = 5;
+let k = 8;
+j = j + k;
+k = j - k;
+j = j - k;
+
+console.log(j,k); // j: 8, k: 5
+
+
+
+// Que-2 Javascript program find to square.
+
+let x = 24;
+
+console.log(x*x); // x: 576
+
+
+
+// Que-3 Find the therd angle of a right triangle if to others angle are given.
+
+let total,c,bside=30,aside=90;
+c=aside+bside;
+
+total = 180-c;
+
+console.log(total); tatal: 60
