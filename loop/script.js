3939707cef0bf50093f984530e16a562cb756e66
pokaljainam24 <pokaljainam@gmail.com>
2024-06-21 13:31:57 +0530

    done

--- loop/script.js
+++ loop/script.js
@@ -5,3 +5,13 @@ for(i=0; i<=10; i++){
 for(i=10; i>=0; i--){
     console.log(i);
 }
+
+
+for (i=1; i<=5; i++){
+    str = '';
+    for(j=1; j<=i; j++){
+        str += '* ';
+    }
+    console.log(str);
+}
+
