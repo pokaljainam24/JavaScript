8a257393038db55b76e24f480665ff9677bed475
pokaljainam24 <pokaljainam@gmail.com>
2024-06-22 08:33:06 +0530

    done

---
+++ 30Days javascript/03-day-boolean/script.js
@@ -0,0 +1,21 @@
+// Day-03
+
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+// Declare variables and assign values
+let firstName = "John";
+let lastName = "Doe";
+let country = "USA";
+let city = "New York";
+let age = 30;
+let isMarried = false;
+let year = 2024;
+
+// Use the typeof operator to check data types
+console.log("firstName:", typeof firstName); // string
+console.log("lastName:", typeof lastName); // string
+console.log("country:", typeof country); // string
+console.log("city:", typeof city); // string
+console.log("age:", typeof age); // number
+console.log("isMarried:", typeof isMarried); // boolean
+console.log("year:", typeof year); // number
