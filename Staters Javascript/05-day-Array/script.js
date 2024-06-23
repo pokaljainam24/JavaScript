122992880a51a5aecb1fcf8e05e3e111b7f5c92c
pokaljainam24 <pokaljainam@gmail.com>
2024-06-23 15:44:28 +0530

    done

--- Staters Javascript/05-day-Array/script.js
+++ Staters Javascript/05-day-Array/script.js
@@ -2,3 +2,10 @@
 
 // ## Array
 
+// An array is a collection of elements of the same data type stored in contiguous memory Location.
+
+// In JavaScript, arrays are zero-indexed, meaning the first element is at index 0.
+
+// Arrays can be used to store a collection of data, and they provide a way to access and
+// manipulate the data using an index or key.
+
