1d0fb96c66501ed0188c41c4fb0d2add1db0c2c7
pokaljainam24 <pokaljainam@gmail.com>
2024-07-04 12:15:26 +0530

    done

--- Array/sscript.js
+++ Array/sscript.js
@@ -76,3 +76,6 @@ const ComName = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "A
 const slicecompanies2 = ComName.slice(-4, -1);
 console.log("Slice:", slicecompanies2);
 
+let a = [1,2,3,4,5,6,7,8,9,7,9,2,4,8];
+let b = a.length;
+console.log(b);
