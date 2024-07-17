680cddb2d4d9f05535f6d87dfa7090e1a19f0361
pokaljainam24 <pokaljainam@gmail.com>
2024-07-17 11:32:26 +0530

    work done

---
+++ 30Days of JavaScript/07-day-Function/function.js
@@ -0,0 +1,36 @@
+// # 📔 Day 7
+
+// ## Functions
+
+// A function is a reusable block of code or programing statements designed to perform a certain TextTrackList.
+
+// There are 4 types of functions how to created
+
+// 1. Declaration function
+// 2. Expression function
+// 3. Anonymous function
+// 4. Arrow function
+
+
+// ### Function Declaration ### //
+
+function square(){
+    console.log(5*5);
+}
+square(); // 25
+
+function square1(){
+    let num = 8;
+    let sq = num * num;
+    console.log(sq);
+}
+square1(); // 64
+
+function printName (){
+    let firstname = 'Jainam';
+    let lastname = 'Pokal';
+    console.log(firstname + ' ' + lastname);
+}
+printName();
+
+
