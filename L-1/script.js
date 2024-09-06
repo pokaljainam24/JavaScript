596968353409ffa4e10226d1316ac0ad465cc081
pokaljainam24 <pokaljainam@gmail.com>
2024-09-06 14:47:50 +0530

    work done

--- L-1/script.js
+++ L-1/script.js
@@ -1,14 +1,14 @@
-var str1 = 'Hello, World!';   //string function
+// var str1 = 'Hello, World!';   //string function
 
 
 
-var a=47;  // positive integer value
-var b=629.5;  // floating point
-var c=-5; // number with negative value
+// var a=47;  // positive integer value
+// var b=629.5;  // floating point
+// var c=-5; // number with negative value
 
 
 
- x = 25;
+//  x = 25;
 // var y = -25;
 // var z = 0;
 // alert(x/z);  // returns Infinity
@@ -40,4 +40,9 @@ var c=-5; // number with negative value
 // console.log(undefined == null) // false
 // console.log(undefined === null) // false
 
-console.log((0.1 + 0.2) === 0.3)
+// console.log((0.1 + 0.2) === 0.3)
+
+let a = 5 > 4;
+let b = 4 < 5;
+let c = a >= b;
+console.log(c); // true
