56cedbeadc83b00fa10caf56143ae12e34a8b1ff
pokaljainam24 <pokaljainam@gmail.com>
2024-09-08 08:53:26 +0530

    work done

--- project-1(Basics calculation)/calculation.js
+++ project-1(Basics calculation)/calculation.js
@@ -1,33 +1,62 @@
 // print only name using prompt with basic calculation in js.
 
-let name = prompt("Enter your name: ");
-alert(`Hello...${name}...!`);
+// let name = prompt("Enter your name: ");
+// alert(`Hello...${name}...!`);
 
-let num1 = parseFloat(prompt("Enter the first number:"));
-let num2 = parseFloat(prompt("Enter the second number:"));
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
 
-let operator = prompt("Enter the operator (+, -, *, /, %):");
-let result = 0;
+// let operator = prompt("Enter the operator (+, -, *, /, %):");
+// let result = 0;
 
-switch (operator) {
-    case "+":
-        result = num1 + num2;
-        break;
-    case "-":
-        result = num1 - num2;
-        break;
-    case "*":
-        result = num1 * num2;
-        break;
-    case "/":
-        result = num1 / num2;
-        break;
-    case "%":
-        result = num1 % num2;
-        break;
-    default:
-        console.log("Invalid operator");
-        break;
+// switch (operator) {
+//     case "+":
+//         result = num1 + num2;
+//         break;
+//     case "-":
+//         result = num1 - num2;
+//         break;
+//     case "*":
+//         result = num1 * num2;
+//         break;
+//     case "/":
+//         result = num1 / num2;
+//         break;
+//     case "%":
+//         result = num1 % num2;
+//         break;
+//     default:
+//         alert("Invalid operator");
+//         break;
+// }
+
+// alert(result.toFixed(2));
+
+
+// Type-2
+
+let name1 = prompt("Enter your name: ");
+alert(`Hello...${name1}...!`);
+
+let num3 = parseFloat(prompt("Enter the first number:"));
+let num4 = parseFloat(prompt("Enter the second number:"));
+
+let operators = prompt("Enter the operator (+, -, *, /, %):");
+let results = 0;
+
+if (operators === "+") {
+    results = num3 + num4;
+} else if (operators === "-") {
+    results = num3 - num4;
+} else if (operators === "*") {
+    results = num3 * num4;
+} else if (operators === "/") {
+    results = num3 / num4;
+} else if (operators === "%") {
+    results = num3 % num4;
+} else {
+    alert("Invalid operator");
 }
 
-alert(result.toFixed(2));
+alert(results.toFixed(2));
+
