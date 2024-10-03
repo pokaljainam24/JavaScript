6d4e35d01aecb584eadf4cfbcbfa23909cff0f72
pokaljainam24 <pokaljainam@gmail.com>
2024-10-03 13:50:08 +0530

    work done

--- demo/script.js
+++ demo/script.js
@@ -62,3 +62,24 @@ let newarr = myarr.filter(removeodd)
 console.log(newarr)                          
 
 
+function adddata(c){
+    if (c % 2 !== 0){
+        return c + 10 * 2 / 3                      
+    }
+}
+let myarr1 = [1,2,3,4,5]      
+let newarr1 = myarr1.map(adddata)
+console.log(newarr1)
+
+
+CompaniesName1 = ['facebook','google','IBM','apple','jainam','jigar','isha','vinni']
+
+const companiesWithoutLast = [CompaniesName1];
+companiesWithoutLast.splice(2, 4);
+console.log("splice company:", companiesWithoutLast);
+
+const ComName = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+const slicecompanies2 = ComName.pop(...ComName)
+ComName.splice(2, 3);
+console.log("pop:", slicecompanies2);
+
---
+++ demo/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+const ComName = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
